093256ff27cdca19187e9d771df85aa3cb9aee37
Michael duBois <mdubois@bytement.com>
2023-09-05 22:45:58 -0600

    Add Initial Svelte App with Static GitHub Pages

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+  <div class="space-y-5">
+    <h1 class="h1">2023 Spud Slappin' Showcase</h1>
+    <p>October, 13th - 15th</p>
+  </div>
+</div>
